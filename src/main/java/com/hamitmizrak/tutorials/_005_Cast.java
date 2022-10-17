2725d11ab46f0ea2c3221dc0a88cf30f3f37f4dc
hamitmizrak <hamitmizrak@gmail.com>
2022-10-17 20:54:16 +0300

    java core continue...

--- src/main/java/com/hamitmizrak/tutorials/_005_Cast.java
+++ src/main/java/com/hamitmizrak/tutorials/_005_Cast.java
@@ -7,5 +7,15 @@ public class _005_Cast {
 
        int primitive=44;
        Integer wrapper=primitive;
+
+       String data="44";
+       int number1=Integer.valueOf(data);
+       int number2=Integer.parseInt(data);
+        System.out.println(number1+16);
+        System.out.println(number2+16);
+
+        int value=15;
+        String data2=String.valueOf(value);
+        String data3=Integer.toString(value);
     }
 }
