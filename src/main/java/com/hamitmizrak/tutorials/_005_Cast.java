e1392978b62054d1ad7a0f711ccc49375853dd8c
hamitmizrak <hamitmizrak@gmail.com>
2022-10-17 20:08:25 +0300

    String , cast , Scanner , StringBuilder

---
+++ src/main/java/com/hamitmizrak/tutorials/_005_Cast.java
@@ -0,0 +1,11 @@
+package com.hamitmizrak.tutorials;
+
+public class _005_Cast {
+    public static void main(String[] args) {
+       float f1=14.53f;
+       long l2=15515151515515L;
+
+       int primitive=44;
+       Integer wrapper=primitive;
+    }
+}
