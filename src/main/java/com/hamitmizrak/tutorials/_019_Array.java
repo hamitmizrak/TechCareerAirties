b00978e1eb4ae9308bbd5f317bababb4f70b216e
hamitmizrak <hamitmizrak@gmail.com>
2022-10-18 18:10:35 +0300

    Array

--- src/main/java/com/hamitmizrak/tutorials/_019_Array.java
+++ src/main/java/com/hamitmizrak/tutorials/_019_Array.java
@@ -1,7 +1,7 @@
 package com.hamitmizrak.tutorials;
 
 import java.util.Arrays;
-//0-10 arasında 5 tane random sayı oluturup tek boyutlu diziye atayalım ?
+// 0-10 arasında 5 tane random sayı oluturup tek boyutlu diziye atayalım ?
 // bu sayıların toplamı
 // bu sayılar çift kaç tane ve toplamları ?
 // metot yaparak ilerliyelim ?
