c791606cb1d9b17774f2f5fe89a0c11c6bc72739
hamitmizrak <hamitmizrak@gmail.com>
2022-10-19 18:19:14 +0300

    forEach

--- src/main/java/com/hamitmizrak/tutorials/_024_StreamApi1.java
+++ src/main/java/com/hamitmizrak/tutorials/_024_StreamApi1.java
@@ -17,14 +17,14 @@ import java.util.List;
 public class _024_StreamApi1 {
 
     //Array
-    public static String[] strArray(){
-        String[] arrayData={"ankara","malatya","istanbul","eskişehir","sivas","malatya","elazığ","malatya"};
+    public static String[] strArray() {
+        String[] arrayData = {"ankara", "malatya", "istanbul", "eskişehir", "sivas", "malatya", "elazığ", "malatya"};
         return arrayData;
     }
 
     //Collection
-    public static List<String> collectionListData(){
-        List<String> strList=new ArrayList<>();
+    public static List<String> collectionListData() {
+        List<String> strList = new ArrayList<>();
         strList.add("ankara");
         strList.add("malatya");
         strList.add("istanbul");
@@ -36,8 +36,33 @@ public class _024_StreamApi1 {
         return strList;
     }
 
+    //Array'ı => Liste çevir
+    public static List<String> arrayToList() {
+        return  List.of(strArray());
+    }
+
+    public static void streamForEachMethod(){
+        //List<String> list= collectionListData();
+        List<String> list= arrayToList();
+        for (int i = 0; i <list.size() ; i++) {
+            System.out.print(list.get(i)+" ");
+        }
+        System.out.println("\n*******************");
+        //forEach
+        /*System.out.println("\n*******************");
+        list.stream().forEach(System.out::println);
+        System.out.println("\n*******************");
+        //method referances
+        list.forEach(System.out::println);*/
+
+        //forEach Lambda Expression
+        list.forEach( (temp)->{
+            System.out.print(temp.toUpperCase().substring(0,3)+" ");
+        });
+    }
+
     //PSVM
     public static void main(String[] args) {
-
+        streamForEachMethod();
     }
 }
